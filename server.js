7307622064d4ca1da3e1c97161d643a48afdc802
islam alghoul <islamalghoul1997>
2022-09-04 21:42:36 +0300

    lab11

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 'use strict';
-
+let book=require('./mongo')
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-
 const app = express();
 app.use(cors());
+
 
 const PORT = process.env.PORT || 3001;
 
@@ -14,5 +14,22 @@ app.get('/test', (request, response) => {
   response.send('test request received')
 
 })
+app.get('/',(req,res)=>{
+  res.send('home route request received')
+})
+app.get('/books',(req,res)=>{
 
+  book.find({},(err,result)=>{
+    if(err)
+    {
+        console.log(err);
+    }
+    else
+    {
+        console.log(result);
+        res.send(result);
+    }
+})
+})
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
+// seedData()
